6ad247d6af93bfe50052ec715e4743d3aa6ee381
alexKosnev <96249469+alexKosnev@users.noreply.github.com>
2021-12-16 18:38:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 import aiohttp
 from aiohttp import web
-import os
-
-TOKEN = "7e03be3a7a811b10d93a62d43e119d09fd6e1d4fdb30b7f9de98da001270"
-HOST_PORT = int(os.environ.get('HOST_PORT', '8443'))
-updater = Updater(TOKEN)
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=HOST_PORT,
-                      url_path=TOKEN,
-                      webhook_url="https://test-marusya.herokuapp.com/" + TOKEN)
-updater.idle()
-
 
 HOST_IP = "0.0.0.0"
+HOST_PORT = 8080
 
-async def skill_space(request_obj):
+async def test-marusya(request_obj):
     request = await request_obj.json()
     
     response = {}
@@ -31,7 +20,7 @@ async def skill_space(request_obj):
     
 def init():
     app = web.Application()
-    app.router.add_post("/skill_space", skill_space)
+    app.router.add_post("/test-marusya", test-marusya)
     web.run_app(app, host = HOST_IP, port = HOST_PORT)
     
 if __name__ == "__main__":
