f052850c00c88127b3b350e641b79367105bab4b
alexKosnev <96249469+alexKosnev@users.noreply.github.com>
2021-12-16 18:14:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import aiohttp
 from aiohttp import web
+import os
 
-HOST_IP = "127.0.0.1"
-HOST_PORT = 3000
+TOKEN = "7e03be3a7a811b10d93a62d43e119d09fd6e1d4fdb30b7f9de98da001270"
+HOST_PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN)
+# add handlers
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN,
+                      webhook_url="https://test-marusya.herokuapp.com/" + TOKEN)
+updater.idle()
+
+
+HOST_IP = "0.0.0.0"
 
 async def skill_space(request_obj):
     request = await request_obj.json()
